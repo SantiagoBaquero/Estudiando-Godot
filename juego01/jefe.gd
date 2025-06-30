8e59759e9628dc1c4d6ab2413b406821ad8ce8db
Santiago Andres Baquero Cortes <baquerosantiago12@gmail.com>
2025-06-30 12:39:34 -0500

    Modificaciones animacion

--- juego01/jefe.gd
+++ juego01/jefe.gd
@@ -1,16 +1,17 @@
 class_name jefe
 extends Node2D
 
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_player= $AnimationPlayer
 
 const ESCENA_SAW = preload("res://bomb.tscn")
 
-func _process(delta):
-	animation_player.play("lanzar")
-	
 	
 func launch_saw():
 	var instancia_saw = ESCENA_SAW.instantiate()
-	instancia_saw.position =$Bomb4.position
+	instancia_saw.position =$BarnacleAttackA/SnailShell/Bomb4.position
 	add_child(instancia_saw)
+	animation_player.play("Reposo")
+
+func _on_timer_timeout():
+	animation_player.play("Lanzar")
+	$Timer.wait_time = randf_range(2,5)
