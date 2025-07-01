9f4a97048d7fe42832ae8c53c23641a493704056
Santiago Andres Baquero Cortes <baquerosantiago12@gmail.com>
2025-06-30 19:05:17 -0500

    Arreglar

--- juego01/jefe.gd
+++ juego01/jefe.gd
@@ -2,15 +2,14 @@ class_name jefe
 extends Node2D
 
 @onready var animation_player= $AnimationPlayer
-
 const ESCENA_BOMB = preload("res://bomb.tscn")	
+
+func _process(delta):
+	animation_player.play("Reposo")
+	launch_bomb()
 
 func launch_bomb():
 	var instancia_bomb = ESCENA_BOMB.instantiate()
 	instancia_bomb.position =$BarnacleAttackA/SnailShell.position
 	add_child(instancia_bomb)
-	animation_player.play("Reposo")
-
-func _on_timer_timeout():
-	animation_player.play("Lanzar")
-	$Timer.wait_time = randf_range(2,5)
+	
